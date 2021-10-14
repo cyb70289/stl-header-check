9b197d8cd6d822593ef3326352a8ebd9ff105687
Yibo Cai <cyb70289@gmail.com>
2021-10-14 17:28:54 +0800

    remove redundant header check
    Too many special cases make redundant check not very useful in practice.

--- analyse-cpp.py
+++ analyse-cpp.py
@@ -22,21 +22,10 @@ def get_stl_funcs_headers(fn):
         func = token[5:]
         funcs.add(func)
 
-    #print('headers: {}'.format(sorted(headers)))
-    #print('funcs: {}'.format(sorted(funcs)))
+    if verbose:
+        print('headers: {}'.format(sorted(headers)))
+        print('funcs: {}'.format(sorted(funcs)))
     return funcs, headers
-
-
-# symbols in these headers may be used without std:: prefix
-# e.g., c (memcpy, int32_t), iostream (<<, >>), ...
-def get_nostd_headers():
-    nostd_headers = \
-        '''
-        cassert cctype cerrno cfenv cfloat cinttypes ciso646 climits clocale
-        cmath csetjmp csignal cstdalign cstdarg cstdbool cstddef cstdint cstdio
-        cstdlib cstring ctgmath ctime cuchar cwchar cwctype iostream
-        '''
-    return set(nostd_headers.split())
 
 
 verbose = '-v' in sys.argv
@@ -49,8 +38,6 @@ for fn in sys.argv[1:]:
     funcs, headers = get_stl_funcs_headers(fn)
 
     missing_headers = []
-    # exclude headers including funcs can be called without std::
-    redundant_headers = headers - get_nostd_headers()
 
     for func in funcs:
         if func not in funcs_to_headers:
@@ -62,17 +49,13 @@ for fn in sys.argv[1:]:
         # make sure at least one of them is included
         if not (headers & required_headers):
             missing_headers.append((func, required_headers))
-        redundant_headers -= required_headers
 
-    if not missing_headers and not redundant_headers:
-        continue
-
-    print('FILE:', fn)
     if missing_headers:
+        print('FILE:', fn)
+        print('++++++++++ MISSING HEADERS ++++++++++'.format(fn))
         # sort missing headers by len(required_headers)
         missing_headers = sorted(missing_headers, key=lambda x: len(x[1]))
         headers_so_far = set()
-        print('++++++++++ MISSING HEADERS ++++++++++'.format(fn))
         for func, required_headers in missing_headers:
             if not (headers_so_far & required_headers):
                 print('+#include', end='')
@@ -80,9 +63,4 @@ for fn in sys.argv[1:]:
                     print(' <{}>'.format(header), end='')
                 headers_so_far = headers_so_far.union(required_headers)
                 print('\t// std::{}'.format(func))
-    # redundant header checking is a bit noisy, hide it if not verbose mode
-    if redundant_headers and verbose:
-        print('---------- UNUSED HEADERS ----------'.format(fn))
-        for header in redundant_headers:
-            print('-#include <{}>'.format(header))
-    print('\n')
+        print('\n')
